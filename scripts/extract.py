7bf5d4040a1129a32eaa48808d3141d500a3a9df
dorothy <dorothycheruiyot97@gmail.com>
2022-03-27 21:17:02 +0300

    additional function added

--- scripts/extract.py
+++ scripts/extract.py
@@ -2,9 +2,15 @@ import pandas as pd
 import csv
 import requests
 
-def view_file(path: str):
+path='https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2021-01.csv'
+def view_file():
     """function that takes in the file-path and returns the first 5 rows of the csv file"""
     file=pd.read_csv(path)
     file=file.head()
     return file
 
+def main():
+    print("[Extract] Start...")
+    print("[Extract] View the first 5 lines in the file...")
+    view_file()
+    print("[Extract] End...")
