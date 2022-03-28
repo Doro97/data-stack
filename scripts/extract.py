0bbc29d37d5c18135b5abd876727b5b58231bae0
dorothy <dorothycheruiyot97@gmail.com>
2022-03-28 13:16:46 +0300

    function modified

--- scripts/extract.py
+++ scripts/extract.py
@@ -1,16 +1,21 @@
 import pandas as pd
 import csv
-import requests
+# import requests
 
-path='https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_2021-01.csv'
+
+path='E:\Data engineering projects\data-stack\data-stack\yellow_tripdata_2021-01 (1).csv'
 def view_file():
-    """function that takes in the file-path and returns the first 5 rows of the csv file"""
-    file=pd.read_csv(path)
-    file=file.head()
-    return file
+    """function that takes in the file-path and returns the first row of the csv file"""
+    with open (path, mode='r',encoding="windows-1252") as csv_file:
+        reader=csv.DictReader(csv_file)
+        # get the first row
+        row=next(reader)
+        print("[Extract] Printing the first row: ", row)        
+
 
 def main():
-    print("[Extract] Start...")
-    print("[Extract] View the first 5 lines in the file...")
+    print("[Extract] Start...")   
     view_file()
     print("[Extract] End...")
+
+  
